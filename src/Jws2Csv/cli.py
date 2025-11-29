5acf4af0f501201b7e50d8bc38aeddc1045d28bd
T. OKAMURA <muryou180onibi@gmail.com>
2025-11-29 21:51:18 +0900

    I organized the cli for myself.

--- src/Jws2Csv/cli.py
+++ src/Jws2Csv/cli.py
@@ -1,23 +1,40 @@
 import os
+from typing import List, Tuple
 from extractor import Extractor
 
 
-# read all files in jws folder
-def main():
+def main() -> None:
+    """
+    Read all JWS files from 'JWS' folder, extract data, and save as CSV.
+    """
     for filename in os.listdir('JWS'):
         if filename.endswith('.jws'):
             print(filename)
+
+            # Create an instance of Extractor
             extractor = Extractor("JWS/"+filename)
+
+            # Read the header
             extractor.read_header()
+
+            # Read the data
             unpacked_data = extractor.read_data()
+
+            # Transpose data to group by columns
             csv_data = list(zip(*unpacked_data))
+
+            # Write to CSV
+            output_path: str = 'CSV/' + filename + '.csv'
             with open('CSV/'+filename+'.csv', 'w') as f:
+                
                 # write header file 1st is the sample name, 2nd is the comment
                 f.write(extractor.sample_name + ',' + '' + '\n')
                 f.write(extractor.comment + ',' + '' + '\n')
                 f.write(filename.split('.jws')[0] + ',' + '' + '\n')
                 f.write('Wavelength,Absorbance' + '\n')
                 f.write('nm,au' + '\n')
+
+                # Write data rows
                 for row in csv_data:
                     f.write(','.join(map(str, row)) + '\n')
 
